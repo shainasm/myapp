8ada6e90833ea8fb2686eeb4ea5952ad44a7105c
shainasm <smulani2002@google.com>
2021-10-28 06:11:39 -0700

    my app

---
+++ myapp/src/App.js
@@ -0,0 +1,17 @@
+function APP() {
+  return(
+    <div className="APP">
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+     <h4>heading4</h4>
+     <h4>heading4</h4>
+     <h4>heading4</h4>
+     <h4>heading4</h4>
+     <h4>heading4</h4>
+
+    </div>
+  );
+}
+
+export default APP;
+
+// this file is'nt html file, it is jsx file which looks like an html file
