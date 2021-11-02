8fb039b58f022b5456db2c402555a528703ee849
shainasm <smulani2002@google.com>
2021-11-02 07:42:33 -0700

    API call

--- myapp/src/components/Content.js
+++ myapp/src/components/Content.js
@@ -1,36 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 
 const Content = () => {
 
-    const [items, setItems] = useState([])
-    const [value, setValue] = useState('')
+  const [counter, setCounter] = useState(0)
+  const [content, setContent] = useState([])
 
-    const handleSubmit = () => {
-      setItems(prev => [...prev,value])/*this is called the spread operator*/
-      setValue('')
-    }
+  const APICall = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+      const data = await res.json()
+      setContent(data)
+  }
 
-    const handleInput = (e) => {
-      setValue(e.target.value)
-    }
+  useEffect(() => {
+      APICall()
+  }, [])
 
-    const handleDelete = (item) => {
-      console.log(item)
-      setItems(prev => prev.filter(i => i !== item))
-    }
+  useEffect(() => {
+    console.log('counter changed')
+  }, [counter])
 
-
-    return (
-      <>
-        <h2>Todo</h2>
-           
-       <input value={value} onChange={handleInput} type="text" />
-        <button onClick={handleSubmit} type="submit">Add</button>
-          <ul>
-            {items.map(item => <li>{item} <button onClick={() => { handleDelete(item) }}>Delete</button></li>)} 
-          </ul>
-      </>
-    );
+  return (
+    <div>
+      <h2>{counter}</h2>
+      <button onClick={() => { setCounter(prev => prev + 1) }} >Click Me </button>
+        <ul>
+          {content.map(item => <li style={{ margin: "10px", border: "2px solid black" }} >{item.title}</li>)}
+        </ul>
+    </div>);
 }
- 
+
 export default Content;
